1f90008ab98e784ad2176f46cc7634a18aab0c14
Prakhar <prakhar2788@gmail.com>
2024-01-19 15:11:20 +0530

    [Prakhar] Add: Adding Use Case 4 Program

--- Employee_Payroll/Employee_Payroll/Program.cs
+++ Employee_Payroll/Employee_Payroll/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC3
+namespace UC4
 {
     internal class Program
     {
@@ -12,20 +12,20 @@ namespace UC3
         {
             Random rnd = new Random();
             int val = rnd.Next(3);
-            if (val == 0)
+            switch (val)
             {
-                Console.WriteLine("Absent");
-            }
-            else if (val == 1)
-            {
-                Console.WriteLine("Part Time");
-            }
-            else
-            {
-                Console.WriteLine("Full Time");
-
+                case 0:
+                    Console.WriteLine("Absent");
+                    break;
+                case 1:
+                    Console.WriteLine("Part Time");
+                    break;
+                case 2:
+                    Console.WriteLine("Full Time");
+                    break;
             }
             Console.ReadKey();
+
         }
     }
 }
