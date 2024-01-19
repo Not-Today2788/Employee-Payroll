0c77a7072e233b8a4342230cab82162a2303b8d3
Prakhar <prakhar2788@gmail.com>
2024-01-19 15:14:39 +0530

    [Prakhar] Add: Adding Use Case 6 Program

--- Employee_Payroll/Employee_Payroll/Program.cs
+++ Employee_Payroll/Employee_Payroll/Program.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC5
+namespace UC6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int val = 20 * 8 * 20;
-            Console.WriteLine(val);
-            Console.ReadLine();
+            Random rnd = new Random();
+            int days = 0, hours = 0;
+            while (days < 20 && hours < 100)
+            {
+                int val = rnd.Next(3);
+                hours += val * 4;
+                days++;
+                Console.WriteLine($"val : {val}  Hours : {hours} days : {days}");
+            }
+            int wages = hours * 20;
+            Console.WriteLine($"Wages : {wages}");
+            Console.ReadKey();
         }
+
     }
 }
